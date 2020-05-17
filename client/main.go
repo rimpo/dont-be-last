23759e09f672a108c71e885b45acee51301f9984
dh-manoj <60648875+dh-manoj@users.noreply.github.com>
2020-05-17 15:17:18 +0200

    Design game screen (#16)
    * api schema
    
    * api schema update
    
    * fixed the api schema
    
    * player info in game state added
    
    * api schema of move
    
    * player move summary added
    
    * authservice and gameservice for cli
    
    * get current state
    
    Co-authored-by: rimpo <manojbisht5@gmail.com>

--- client/main.go
+++ client/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"github.com/rimpo/dont-be-last/pkg/service/cli/"
+	"github.com/rimpo/dont-be-last/pkg/service/cli/auth"
+	"github.com/rimpo/dont-be-last/pkg/service/cli/game"
 )
 
-func main() {
-	app := app.New()
+type GameWindow struct {
+	window      fyne.Window
+	authService cli.AuthService
+	gameService cli.GameService
+}
 
-	w := app.NewWindow("Hello")
-	w.SetContent(widget.NewVBox(
+func NewGameWindow(authService cli.AuthService, gameService cli.GameService) *GameWindow {
+	gw := &GameWindow{
+		window:      app.NewWindow("Hello"),
+		authService: authService,
+		gameService: gameService,
+	}
+	gw.window = app.NewWindow("Hello")
+
+	gw.window.SetContent(widget.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
 		widget.NewButton("Quit", func() {
 			app.Quit()
 		}),
 	))
+	return gw
+}
+func (gw *GameWindow) Run() {
+	gw.window.ShowAndRun()
+}
 
-	w.ShowAndRun()
+func main() {
+	app := app.New()
+
+	authService := auth.NewService("http://127.0.0.1:5000/auth")
+	gameService := game.NewService()
+
+	gameWindow = NewGameWindow(authService, gameService)
+
+	gameWindow.Run()
 }
---
+++ pkg/service/cli/game/game_service.go
@@ -0,0 +1,20 @@
+package game
+
+import (
+	"github.com/rimpo/dont-be-last/pkg/api"
+)
+
+type service struct {
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (g *service) GetCurrentState(gameId int) *api.GameState {
+	return nil
+}
+
+func (g *service) Move(gameId int, move []int) *api.GameState {
+	return nil
+}
